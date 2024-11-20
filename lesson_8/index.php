b7ae6d6ec2f9946cedd2a4f0caf330e928150301
AmarBytyqi <amar.bytyci@gmail.com>
2024-11-20 17:21:10 +0100

    fnf

--- lesson_8/index.php
+++ lesson_8/index.php
@@ -5,17 +5,17 @@ $user = 'root';
 $pass = '';
 
 try{
-    $pdo = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
+$pdo = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
 
-    $dql = "CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL)";
+   $sql = "CREATE TABLE users(id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   username VARCHAR(20) NOT NULL,
+   password VARCHAR(20) NOT NULL)";
 
-    $pdo => exec($sql);
+   $pdo -> exec($sql);
 
-    echo "Table created successfully";
+   echo "Table created successfully";
 }catch(exeption $e){
 
-    echo "Error while creating the table".$e.getMessage();
+ echo "Error while creating the table".$e.getMessage();
 }
 ?>
